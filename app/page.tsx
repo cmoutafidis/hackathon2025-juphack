22c19e0dedbff08790e2ff97e33e04062ebf5a42
root <root@LAPTOP-1G9A5UCB.localdomain>
2025-06-21 09:51:55 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Voice from "./components/Voice";
+import ChatbotUI from "./components/UI/chat";
 
 export default function Home() {
   return (
    <div>
     <Voice/>
+    <ChatbotUI/>
    </div>  );
 }
